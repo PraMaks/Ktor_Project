45edea2adf8719132c7863c4c6c1e37898b241cc
Pravdin Maksym <maks111111112@gmail.com>
2022-08-29 17:25:14 -0400

    Added mongodb and ADD Co-authored-by: RENAUD Vincent <Vincent-Pisano@users.noreply.github.com>

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -1,6 +1,14 @@
 import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.Id
 
 @Serializable
-data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
+data class Customer(
+    @BsonId
+    val id: Id<String>? = null,
+    val firstName: String,
+    val lastName: String,
+    val email: String
+    )
 
 val customerStorage = mutableListOf<Customer>()
--- src/main/kotlin/com/example/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/example/routes/CustomerRoutes.kt
@@ -1,18 +1,25 @@
 package com.example.routes
 
 import Customer
-import customerStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.bson.types.ObjectId
+import org.litote.kmongo.*
+import org.litote.kmongo.id.toId
 
 fun Route.customerRouting() {
+    val client = KMongo.createClient()
+    val database = client.getDatabase("ktor_project")
+    val collection = database.getCollection<Customer>()
+
     route("/customer") {
         get {
-            if (customerStorage.isNotEmpty()) {
-                call.respond(customerStorage)
+            val customerList = collection.find().toList()
+            if (customerList.isNotEmpty()) {
+                call.respond(customerList)
             } else {
                 call.respondText("No customers found", status = HttpStatusCode.OK)
             }
@@ -23,20 +30,22 @@ fun Route.customerRouting() {
                 status = HttpStatusCode.BadRequest
             )
             val customer =
-                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                collection.findOneById(ObjectId(id).toId<String>()) ?: return@get call.respondText(
                     "No customer with id $id",
                     status = HttpStatusCode.NotFound
                 )
+
             call.respond(customer)
         }
         post {
             val customer = call.receive<Customer>()
-            customerStorage.add(customer)
+            collection.insertOne(customer)
+            /*customerStorage.add(customer)*/
             call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
         }
         delete("{id?}") {
             val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
-            if (customerStorage.removeIf { it.id == id }) {
+            if (collection.deleteOneById(id).wasAcknowledged()){
                 call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
             } else {
                 call.respondText("Not Found", status = HttpStatusCode.NotFound)
